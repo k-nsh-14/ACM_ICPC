23ba457b4adbf619fdf8b46beb36cacb0e8f9f8b
kenshi kamata <kenshi.kamata@gmail.com>
2013-02-08 04:14:27 +0900

    finished red and black

--- red_and_black/red_and_black.py
+++ red_and_black/red_and_black.py
@@ -10,18 +10,21 @@ room_sizeは部屋のサイズ(インデックス調べるように)，リスト
 '''
 def travel(room_info,room_size, point):
 	next_pattern = [[1,0],[0,1],[-1,0],[0,-1]]
-	print room_size
-	print point
-	print room[point[0]][point[1]]
-	if room[point[0]][point[1]] != "#":
-		if point not in black_tile_list:
-			black_tile_list.append(point)
+	#print room_size
+	#print point
+	#print room_info
+	if room_info[point[0]][point[1]] != "#":
+		black_tile_list.append(point)
 		for next in next_pattern:
+			#print next
 			next_point = (point[0]+next[0], point[1]+next[1])
-			if (0 <= next_point[0] < room_size[0]) and (0 <= next_point[1] < room_size[1]):
+			if 0 <= next_point[0] < room_size[0] and 0 <= next_point[1] < room_size[1] and next_point not in black_tile_list:
 				travel(room_info,room_size, next_point)
+			else:
+				pass
 	else:
 		pass
+	return 0
 	#if 自分のとこがOKタイルかどうか
 		# 登録されているかどうか
 			#登録する
@@ -35,7 +38,7 @@ def travel(room_info,room_size, point):
 def find_start(room):
 	for i,l in enumerate(room):
 		if '@' in l:
-			return (l.index('@'), i)
+			return (i,l.index('@'))
 
 
 import sys
@@ -53,14 +56,17 @@ for line in data:
 		start = find_start(room)
 		
 		if start != None:
-			print "start = ",start
+			#print "start = ",start
 			travel(room, room_info,start)
 			print len(black_tile_list)
-		room_info = map(int, line[:-2].split())
-		define_flag = int(room_info[1])
+		tmp_room_info = map(int, line[:-2].split())
+		room_info = [tmp_room_info[1], tmp_room_info[0]]
+		define_flag = int(room_info[0])
 		room = []
 		black_tile_list = []
 	else:
 		room.append(list(line[:-2]))
 		define_flag -= 1
-		
+start = find_start(room)
+travel(room, room_info,start)
+print len(black_tile_list)		
