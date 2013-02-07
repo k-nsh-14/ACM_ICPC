ab40b45aa8d9830b6db149cbf641a6f29182bc8b
kenshi kamata <kenshi.kamata@gmail.com>
2013-02-08 03:49:11 +0900

    update test_e for solve

---
+++ red_and_black/red_and_black.py
@@ -0,0 +1,66 @@
+﻿#!/bin/python
+#-*-coding:utf-8-*-
+
+#ポイントから四方に突っ込んでみて，一つもいけなかったら１を返す，
+'''
+引数の話
+room_infoはその部屋の情報(どこがいけるタイルか)，2次元リスト
+room_sizeは部屋のサイズ(インデックス調べるように)，リスト(要素は2つx座標とy座標)
+ポイントは自分のとこ，(x, y)のタプル
+'''
+def travel(room_info,room_size, point):
+	next_pattern = [[1,0],[0,1],[-1,0],[0,-1]]
+	print room_size
+	print point
+	print room[point[0]][point[1]]
+	if room[point[0]][point[1]] != "#":
+		if point not in black_tile_list:
+			black_tile_list.append(point)
+		for next in next_pattern:
+			next_point = (point[0]+next[0], point[1]+next[1])
+			if (0 <= next_point[0] < room_size[0]) and (0 <= next_point[1] < room_size[1]):
+				travel(room_info,room_size, next_point)
+	else:
+		pass
+	#if 自分のとこがOKタイルかどうか
+		# 登録されているかどうか
+			#登録する
+		#else
+			# pass
+		#四方がindexからはみ出してないかどうか
+			#再起する
+	#else
+		#pass
+
+def find_start(room):
+	for i,l in enumerate(room):
+		if '@' in l:
+			return (l.index('@'), i)
+
+
+import sys
+data = open(sys.argv[1]).readlines()
+
+
+define_flag = 0
+black_tile_list = []
+room = []
+for line in data:
+	if line == '0 0':
+		break
+	if define_flag == 0:
+		#travel(room,point)
+		start = find_start(room)
+		
+		if start != None:
+			print "start = ",start
+			travel(room, room_info,start)
+			print len(black_tile_list)
+		room_info = map(int, line[:-2].split())
+		define_flag = int(room_info[1])
+		room = []
+		black_tile_list = []
+	else:
+		room.append(list(line[:-2]))
+		define_flag -= 1
+		
---
+++ red_and_black/test_e.py
@@ -0,0 +1,30 @@
+﻿# -*- coding:utf-8 -*-
+
+
+def travel(room_info, point):
+	next_pattern = [[1,0],[0,1],[-1,0],[0,-1]]
+	print point[0]," , ",point[1]
+	travel_list.append(point)
+	#print a[point[0]][point[1]]
+	
+	for next in next_pattern:
+		next_point = (point[0]+next[0], point[1]+next[1])
+		if 0 <= next_point[0] < 3 and 0 <= next_point[1] < 3 and next_point not in travel_list:
+		#	next_point = (point[0]+next[0], point[1]+next[1])
+
+			travel(room_info, next_point)
+		else:
+			pass
+		#count += 1
+		#if count == 100:
+		#	break
+	return 0
+travel_list = []
+count = 0
+a = [[1,2,3],[4,5,6],[7,8,9]]
+#try:
+travel(a,(1,1))
+#except Exception as e:
+#    print e.message
+    
+print '処理終了'
